c2c0e79911e2c272233e866179893c49dc7d90c9
YuxinLi1 <yxleezw@163.com>
2020-12-19 19:49:15 +0800

    create table sql

--- be/model/store.py
+++ be/model/store.py
@@ -1,65 +1,119 @@
 import logging
 import os
 import sqlite3 as sqlite
+import pymysql
 
 
 class Store:
-    database: str
+    # database: str
 
-    def __init__(self, db_path):
-        self.database = os.path.join(db_path, "be.db")  # 数据库路径
+    def __init__(self):
+        # self.database = os.path.join(db_path, "be.db")  # 数据库路径
         self.init_tables()
 
     ''' 如果不存在对应的表则创建 '''
     def init_tables(self):
         try:
-            conn = self.get_db_conn()
-            conn.execute(
+            db = self.get_db_conn()
+            cursor = db.cursor()
+            cursor.execute(
                 "CREATE TABLE IF NOT EXISTS user ("
-                "user_id TEXT PRIMARY KEY, password TEXT NOT NULL, "
-                "balance INTEGER NOT NULL, token TEXT, terminal TEXT);"
+                "user_id VARCHAR(100) PRIMARY KEY, "
+                "password TEXT NOT NULL, "
+                "balance INTEGER NOT NULL, "
+                "token TEXT, "
+                "terminal TEXT"
+                ");"
             )
 
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS user_store("
-                "user_id TEXT, store_id, PRIMARY KEY(user_id, store_id));"
-            )
-
-            conn.execute(
+            cursor.execute(
                 "CREATE TABLE IF NOT EXISTS store( "
-                "store_id TEXT, book_id TEXT, book_info TEXT, stock_level INTEGER,"
-                " PRIMARY KEY(store_id, book_id))"
+                "store_id VARCHAR(100), "
+                "book_id VARCHAR(10), "
+                "book_info TEXT, "
+                "stock_level INTEGER,"
+                "PRIMARY KEY(store_id, book_id))"
             )
 
-            conn.execute(
+            cursor.execute(
+                "CREATE TABLE IF NOT EXISTS user_store("
+                "user_id VARCHAR(100), "
+                "store_id VARCHAR(100), "
+                "PRIMARY KEY(user_id, store_id), "
+                "FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE, "
+                "FOREIGN KEY(store_id) REFERENCES store(store_id) ON DELETE CASCADE ON UPDATE CASCADE"
+                ");"
+            )
+
+            cursor.execute(
                 "CREATE TABLE IF NOT EXISTS new_order( "
-                "order_id TEXT PRIMARY KEY, user_id TEXT, store_id TEXT)"
+                "order_id VARCHAR(100) PRIMARY KEY, "
+                "user_id VARCHAR(100), "
+                "store_id VARCHAR(100), "
+                "order_time DATETIME, "
+                "state ENUM('dont', 'doing', 'done'), "
+                "message TEXT, "
+                "FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE, "
+                "FOREIGN KEY(store_id) REFERENCES store(store_id) ON DELETE CASCADE ON UPDATE CASCADE"
+                ");"
             )
 
-            conn.execute(
+            cursor.execute(
+                "CREATE TABLE IF NOT EXISTS unpay_order("
+                "order_id VARCHAR(100), "
+                "order_time DATETIME"
+                ");"
+            )
+
+            cursor.execute(
                 "CREATE TABLE IF NOT EXISTS new_order_detail( "
-                "order_id TEXT, book_id TEXT, count INTEGER, price INTEGER,  "
-                "PRIMARY KEY(order_id, book_id))"
+                "order_id VARCHAR(100), "
+                "book_id VARCHAR(10), "
+                "count INTEGER, "
+                "price INTEGER, "
+                "PRIMARY KEY(order_id, book_id), "
+                "FOREIGN KEY(order_id) REFERENCES new_order(order_id) ON DELETE CASCADE ON UPDATE CASCADE"
+                ");"
             )
 
-            conn.commit()
+            cursor.execute(
+                "CREATE TABLE IF NOT EXISTS book_comment("
+                "user_id VARCHAR(100), "
+                "store_id VARCHAR(100), "
+                "book_id VARCHAR(10), "
+                "comment TEXT, "
+                "rate int, "
+                "PRIMARY KEY(user_id, store_id, book_id), "
+                "FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE, "
+                "FOREIGN KEY(store_id) REFERENCES store(store_id) ON DELETE CASCADE ON UPDATE CASCADE"
+                ");"
+            )
+
+            cursor.commit()
         except sqlite.Error as e:
             logging.error(e)
-            conn.rollback()
+            cursor.rollback()
 
     ''' 连接数据库 '''
-    def get_db_conn(self) -> sqlite.Connection:
-        return sqlite.connect(self.database)
+    def get_db_conn(self):
+        db = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='1234zxcv',
+                             db='bookstore', charset='utf8')
+        # return sqlite.connect(self.database)
+        return db
 
 
 database_instance: Store = None
 
 
-def init_database(db_path):
+def init_database():
     global database_instance
-    database_instance = Store(db_path)
+    database_instance = Store()
 
 
 def get_db_conn():
     global database_instance
     return database_instance.get_db_conn()
+
+
+if __name__ == '__main__':
+    init_database()
